b3947bcd01ffa1d5adc2ed2a3902dc20b5547371
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2024-07-18 21:13:24 +0700

    feat: add root route on go-gin-backend

--- go-gin-backend/main.go
+++ go-gin-backend/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bookweb/controllers"
-	"bookweb/middlewares"
+	middleware "bookweb/middlewares"
 	"bookweb/models"
 
 	"github.com/gin-gonic/gin"
@@ -11,16 +11,24 @@ import (
 func main() {
 	r := gin.Default()
 	models.ConnectDatabase()
+
+	// root route
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to the bookweb API",
+		})
+	})
+
 	v1 := r.Group("/api/v1")
 	{
-		v1.GET("/books",controllers.FindBooks)
-		v1.POST("/books",controllers.CreateBook)
-		v1.GET("/books/:id",controllers.FindBook)
-		v1.PUT("/books/:id",controllers.UpdateBook)
-		v1.DELETE("/books/:id",controllers.DeleteBook)
-		v1.POST("/register",controllers.Register)
-		v1.POST("/login",controllers.Login)
-		v1.GET("/user",middleware.JWTAuthMiddleware(),controllers.CurrentUser)
+		v1.GET("/books", controllers.FindBooks)
+		v1.POST("/books", controllers.CreateBook)
+		v1.GET("/books/:id", controllers.FindBook)
+		v1.PUT("/books/:id", controllers.UpdateBook)
+		v1.DELETE("/books/:id", controllers.DeleteBook)
+		v1.POST("/register", controllers.Register)
+		v1.POST("/login", controllers.Login)
+		v1.GET("/user", middleware.JWTAuthMiddleware(), controllers.CurrentUser)
 	}
 	// health check
 	r.GET("/health/check", func(c *gin.Context) {
@@ -28,6 +36,6 @@ func main() {
 			"status": "true",
 		})
 	})
-	
+
 	r.Run()
 }
